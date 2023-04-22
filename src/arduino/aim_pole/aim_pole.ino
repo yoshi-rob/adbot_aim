d78f3b8224ed6626b52da3d6c6438a6dfdcbe608
yoshi-rob <95674429+yoshi-rob@users.noreply.github.com>
2023-04-22 14:22:31 +0900

    修正版

--- src/arduino/aim_pole/aim_pole.ino
+++ src/arduino/aim_pole/aim_pole.ino
@@ -8,7 +8,7 @@
 #include "PID.h"
 #include "Cubic.controller.h"
 
-double target = 0; // 正面
+double target = 0.0; // 正面
 double capableDuty = 0.1, Kp, Ki, Kd;
 
 ros::NodeHandle nh;
@@ -23,11 +23,11 @@ void messageCb_Kp( const std_msgs::Float64& sub_Kp){
 }
 
 void messageCb_Ki( const std_msgs::Float64& sub_Ki){
-  Kp = sub_Ki.data;
+  Ki = sub_Ki.data;
 }
 
 void messageCb_Kd( const std_msgs::Float64& sub_Kd){
-  Kp = sub_Kd.data;
+  Kd = sub_Kd.data;
 }
 
 std_msgs::Float64 angle_diff;
@@ -60,56 +60,12 @@ void loop()
   using namespace Cubic_controller;
   nh.spinOnce();
   
-  static Velocity_PID velocityPID(3, 0, encoderType::inc, 2048 * 4, 0.5, 0.5, 0.5, 0.1, 0.4, false, false);
-  static Position_PID positionPID(3, 0, encoderType::abs, AMT22_CPR, capableDuty, Kp, Ki, Kd, degToRad(target), true, false);
+  static Position_PID positionPID(7, 0, encoderType::abs, AMT22_CPR, capableDuty, Kp, Ki, Kd, target, true, false);
   static bool stopFlag = false;
   positionPID.setGains(Kp, Ki, Kd);
-  /*
-  if (Serial.available() > 0)
-  {
-    char c = Serial.read();
-    double value = Serial.readStringUntil('\n').toDouble();
-    if (c == 'p')
-    {
-      velocityPID.setKp(value);
-      positionPID.setKp(value);
-    }
-    else if (c == 'i')
-    {
-      velocityPID.setKi(value);
-      positionPID.setKi(value);
-    }
-    else if (c == 'd')
-    {
-      velocityPID.setKd(value);
-      positionPID.setKd(value);
-    }
-    else if (c == 's')
-    {
-      velocityPID.setTarget(value);
-      positionPID.setTarget(degToRad(value));
-    }
-    else
-    {
-      stopFlag = true;
-    }
-  }
-  */
-  if (stopFlag)
-  {
-    // Serial.println("stopping...");
-    for (int i = 0; i < 8; i++)
-    {
-      DC_motor::put(i, 0);
-    }
-  }
-  else
-  {
-    // velocityPID.compute();
-    positionPID.setTarget(degToRad(target));
-    positionPID.compute();
-    duty.data = DC_motor::get(3);
-  }
+  positionPID.setTarget(target);
+  positionPID.compute();
+  duty.data = DC_motor::get(7);
   Cubic::update();
 
   angle_diff.data = abs(positionPID.getTarget() - positionPID.getCurrent());
